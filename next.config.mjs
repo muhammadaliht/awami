b3e8961aa671b978d8a646319b25e4958cff453b
muhammadaliht <muhammadali38906@gmail.com>
2025-04-27 23:11:50 +0500

    Update code before build and deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: { unoptimized: true },
+  eslint: { ignoreDuringBuilds: true },
+  basePath: '/awami', // Add this line to set the base path
+};
+
+export default nextConfig;
