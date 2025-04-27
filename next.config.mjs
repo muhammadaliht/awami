f8e2df24105c631eb7f4b86ef130edc7ff5e8d3c
muhammadaliht <muhammadali38906@gmail.com>
2025-04-27 22:43:23 +0500

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: { unoptimized: true },
-};
-
-export default nextConfig;
+    images: {
+      unoptimized: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+  
+  export default nextConfig;
+  
